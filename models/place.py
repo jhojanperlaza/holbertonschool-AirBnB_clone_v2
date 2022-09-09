64764e8c39019e9824356364fca807ad40c94f05
jhojan <jhojan_perlaza123>
2022-09-09 15:04:31 -0500

    [UPDATED] places

--- models/place.py
+++ models/place.py
@@ -19,14 +19,15 @@ class Place(BaseModel, Base if (os.getenv('HBNB_TYPE_STORAGE') == 'db') else obj
         latitude = Column(Float)
         longitude = Column(Float)
 
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
