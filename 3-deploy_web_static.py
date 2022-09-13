b1e648c1028575e729c1e4a2a3e3dbc03fef1aa4
jhojan <jhojan_perlaza123>
2022-09-13 12:05:02 -0500

    [UPDATED] file 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,7 +3,7 @@
 Creates and distributes an archive
 to your web servers
 """
-from fabric.api import *
+from fabric.api import env
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
@@ -17,5 +17,5 @@ def deploy():
     path = do_pack()
     if not path:
         return False
-    new_deploy = do_deploy(path)
-    return new_deploy
+
+    return do_deploy(path)
