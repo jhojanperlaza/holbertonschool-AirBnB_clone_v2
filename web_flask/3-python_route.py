d18716583b2bcced26072a07fe3d9621fd4c914b
jhojan <jhojan_perlaza123>
2022-09-14 14:39:46 -0500

    [ADD] file 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+and print in two different routes
+"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def main_route():
+    """print simple string"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def second_route():
+    """print simple string"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def third_route(text):
+    """ display “C ” followed by the value= text"""
+    text = text.replace("_", " ")
+    return "C {}".format(escape(text))
+
+
+@app.route("/python/(<text>)", strict_slashes=False)
+def fourth_route(text="is cool"):
+    """ display “C ” followed by the value= text"""
+    text = text.replace("_", " ")
+    return "C {}".format(escape(text))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
